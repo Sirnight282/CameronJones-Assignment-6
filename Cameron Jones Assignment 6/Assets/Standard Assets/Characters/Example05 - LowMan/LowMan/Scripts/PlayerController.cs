baa5da7660a112c1a6587b0634cbc113de52fa2b
Cameron Jones <ctjones0928@yahoo.com>
2021-04-17 16:36:35 -0500

    initial push

---
+++ Cameron Jones Assignment 6/Assets/Standard Assets/Characters/Example05 - LowMan/LowMan/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+public class PlayerController : MonoBehaviour
+{
+    public Camera cam;
+    public NavMeshAgent agent;
+    public ThirdPersonCharacter character;
+    public int score;
+
+    private void Start()
+    {
+        agent.updateRotation = false;
+    }
+
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+        if(agent.remainingDistance > agent.stoppingDistance)
+        {
+            character.Move(agent.desiredVelocity, false, false);
+        }
+        else
+        {
+            character.Move(Vector3.zero, false, false);
+        }
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Pickup")
+        {
+            other.gameObject.SetActive(false);
+            score += 10;
+            Debug.Log("Pickup collected, added 10 points, current score: " + score);
+        }
+    }
+}
